15551916af7faa146d69a639b2cb33f3eb9a45b2
mkirov <mkirov@emis.com>
2019-08-22 17:55:43 +0300

    Initial commit

---
+++ dongerror.py
@@ -0,0 +1,38 @@
+import sys
+import config
+import traceback
+import sys
+
+
+def donger_exception_handler(exc_type, exc_value, exc_traceback):
+
+    new_exception = None
+
+    for exc in exc_type.__mro__:
+
+        exc_type_name = exc.__name__
+
+        if exc_type_name in config.EXCEPTION_DONGERS:
+            new_exception = type(
+                f"{exc_type_name} {config.EXCEPTION_DONGERS[exc_type_name].decode()}",
+                (exc_type,),
+                dict(),
+            )
+
+        if new_exception:
+            break
+
+    f = traceback.format_exception(
+        new_exception,
+        new_exception(*exc_value.args),
+        exc_traceback)
+
+    f[-1] = f[-1].replace(
+        f'{__name__}.{exc_type_name}',
+        exc_type_name)
+
+    sys.stderr.write(''.join(f))
+
+
+sys.excepthook = donger_exception_handler
+
